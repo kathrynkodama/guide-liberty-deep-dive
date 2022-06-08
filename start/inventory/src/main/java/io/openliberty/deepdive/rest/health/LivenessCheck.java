fec22c0899ef4c3f881ac09318129ea3da819e06
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2022-06-08 07:16:28 -0400

    Finish version of liberty deep dive demo
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

---
+++ start/inventory/src/main/java/io/openliberty/deepdive/rest/health/LivenessCheck.java
@@ -0,0 +1,25 @@
+package io.openliberty.deepdive.rest.health;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.health.Liveness;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+@Liveness
+@ApplicationScoped
+public class LivenessCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        MemoryMXBean memBean = ManagementFactory.getMemoryMXBean();
+        long memUsed = memBean.getHeapMemoryUsage().getUsed();
+        long memMax = memBean.getHeapMemoryUsage().getMax();
+
+        return HealthCheckResponse.named("Liveness Check")
+                                  .status(memUsed < memMax * 0.9)
+                                  .build();
+    }
+}
